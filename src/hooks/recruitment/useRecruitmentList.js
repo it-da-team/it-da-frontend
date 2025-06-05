579527c0844d1a503f30294243530b31a398c941
Hojeong016 <skflsehdn@naver.com>
2025-06-05 22:51:09 +0900

    feat 채용공고 조회 및 검색 api 연동

--- src/hooks/recruitment/useRecruitmentList.js
+++ src/hooks/recruitment/useRecruitmentList.js
@@ -10,9 +10,18 @@ export default function useRecruitmentList(categoryEnum) {
   useEffect(() => {
     if (!categoryEnum) return;
 
+    setLoading(true);
+    setError(null);
+
     fetchRecruitmentsByCategory(categoryEnum)
-      .then(setData)
-      .catch((err) => setError(err.message))
+      .then(responseData => {
+        console.log('받은 데이터:', responseData);
+        setData(responseData);
+      })
+      .catch((err) => {
+        console.error('데이터 로딩 에러:', err);
+        setError(err.message);
+      })
       .finally(() => setLoading(false));
   }, [categoryEnum]);
 
--- src/pages/Jop/detail/CompanyDetail.jsx
+++ src/pages/Jop/detail/CompanyDetail.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import "../../../assets/css/Company.css";
 
-function CompanyDetail({ logoUrl , description}) {
+function CompanyDetail({ logoUrl, description, companyUrl }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
-    <h4>{description}</h4>
+      <h4>{description}</h4>
+      
+      {/* 회사 URL */}
+      {companyUrl && (
+        <div className="company-url">
+          <a href={companyUrl} target="_blank" rel="noopener noreferrer">
+            {companyUrl}
+          </a>
+        </div>
+      )}
     
       {/* 로고 (클릭하면 모달 열림) */}
-      <div className="company-img-wrapper" onClick={() => setIsModalOpen(true)}>
-        <img src={logoUrl} alt="회사 로고" className="company-img" />
-      </div>
+      {logoUrl && (
+        <div className="company-img-wrapper" onClick={() => setIsModalOpen(true)}>
+          <img src={logoUrl} alt="회사 로고" className="company-img" />
+        </div>
+      )}
 
       {/* 확대 이미지 모달 */}
       {isModalOpen && (
