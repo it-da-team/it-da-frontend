bcfa240956caa3c8cc920781e974207960ab4684
Hojeong016 <skflsehdn@naver.com>
2025-07-22 18:50:03 +0900

    feat 홈 화면 모바일 배치 조정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,6 @@ import MainCategory from './MainCategory';
 import MainCategoryList from './MainCategoryList';
 import HotRecruitSection from './HotRecruitSection';
 import RegionStatsSection from './RegionStatsSection';
-import StatsBar from './StatsBar';
 import './Home.css';
 
 function Home() {
@@ -16,18 +15,11 @@ function Home() {
       <div className="main-section">
         <MainCategory variant="home" />
       </div>
-      <div className="main-section">
-        <MainCategoryList />
-      </div>
-
       <div className="divider-horizon" />
-      
       <div className="main-section last-section">
         <RegionStatsSection />
       </div>
-      <div className="main-section">
-        <StatsBar />
-      </div>
+    
       <div className="divider-horizon" />
       <div className="main-section">
         <HotRecruitSection />
